35cdefb98da5555a2b90b1adfba170d608cacfc8
Michal Nyga <mnyga94@gmail.com>
2022-01-09 11:09:43 +0100

    Create conditionalStatements.py

---
+++ conditionalStatements.py
@@ -0,0 +1,14 @@
+#!/bin/python
+test=11
+if test > 0:
+    if test > 10 and test < 15:
+        print("Positive in range: 10-15")
+    else:
+        print("Positive")
+elif test < 0:
+    print("Negative")
+else:
+    print("ZERO")
+
+test2 = 0
+if test2 == 0: print("One line!")
